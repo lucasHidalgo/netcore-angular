c7559e59bcdaa24a5481e47abc6466430bf65c8e
Lucas Hidalgo <hidalgolucas95@gmail.com>
2018-11-18 22:57:35 -0300

    Section 9

--- MeetingApp-SPA/src/app/routes.ts
+++ MeetingApp-SPA/src/app/routes.ts
@@ -1,6 +1,10 @@
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
  import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -9,11 +13,12 @@ import { MessagesComponent } from './messages/messages.component';
  export const appRoutes: Routes = [
      {path: '', component: HomeComponent},
      {
-         path: '', // Se deja vacio para que solo matchee los path de los childrens 
+         path: '', // Se deja vacio para que solo matchee los path de los childrens
          runGuardsAndResolvers: 'always',
          canActivate: [AuthGuard],
          children: [
-            {path: 'members', component: MemberListComponent},
+            {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+            {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent},
          ]
