78b75a84d5c3f116ce0f77943dce155cf368fa91
Unknown <hidalgolucas95@hotmail.com>
2019-03-21 23:32:37 -0300

    12

--- MeetingApp-SPA/src/app/app.module.ts
+++ MeetingApp-SPA/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { AlertifyService } from './_services/alertify.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import {NgxGalleryModule} from 'ngx-gallery';
@@ -55,8 +55,10 @@ export function tokenGetter () {
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      BsDatepickerModule.forRoot(),
       NgxGalleryModule,
       FileUploadModule,
+      ReactiveFormsModule,
       JwtModule.forRoot({
           config: {
               tokenGetter: tokenGetter,
--- MeetingApp-SPA/src/app/register/register.component.ts
+++ MeetingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit, Input , EventEmitter, Output } from '@angular/core';
 import { AlertifyService } from '../_services/alertify.service';
+import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,39 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  registerForm: FormGroup;
+  bsConfig: BsDatepickerConfig;
+  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      nombreUsuario: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    },{validator: this.passwordMatchValidity}
+    )
+  }
+
+  passwordMatchValidity(g: FormGroup ) {
+    return g.get('password').value === g.get('confirmPassword') .value ? null : {'missmatch': true};
   }
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('registration successful');
-    },
-    error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('registration successful');
+    // },
+    // error => {
+    //   this.alertify.error(error);
+    // });
+
   }
 
   cancel() {
